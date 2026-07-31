5fe5d656f71e4f104c1dc21b00a08572081e764a
nikhil-lather <nikhillather02687@gmail.com>
2026-07-31 22:51:03 +0530

    updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,21 +2,51 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <header className="fixed-nav-bar w-nav">
-      <nav cl>
+      <nav className="max-w-screen-2xl mx-auto px-4 flex justify-between items-center">
         <ul className="nav__links">
-          <li>
+          <li className="link">
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className="link">
             <Link to="/shop">Shop</Link>
           </li>
-          <li>
+          <li className="link">
             <Link to="/">Pages</Link>
           </li>
-          <li>
+          <li className="link">
             <Link to="/contact">Contact</Link>
           </li>
         </ul>
+        {/*logo*/}
+        <div className="nav__logo">
+          <Link to="/">
+            ShopSphere <span>.</span>
+          </Link>
+        </div>
+        {/*nav icons*/}
+        <div className="nav__icons relative">
+          <span>
+            <Link to="/search">
+              <i className="ri-search-line"></i>
+            </Link>
+          </span>
+          <span>
+            <button className="hover:text-primary">
+              <i className="ri-shopping-cart-line"></i>
+              <sup className="text-sm inline-block px-1.5 text-white rounded-full bg-primary text-center"></sup>{" "}
+            </button>
+          </span>
+          <span>
+            <Link to="/login">
+              <i className="ri-user-line"></i>
+            </Link>
+          </span>
+          <span>
+            <button className="nav__menu-btn">
+              <i className="ri-menu-line"></i>
+            </button>
+          </span>
+        </div>
       </nav>
     </header>
   );
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import router from "./routers/router.jsx";
 import { RouterProvider } from "react-router-dom";
+import "remixicon/fonts/remixicon.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
