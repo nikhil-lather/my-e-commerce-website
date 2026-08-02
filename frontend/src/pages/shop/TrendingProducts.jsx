31430310dbe588eb0a65275213c03a1ea1f26223
nikhil-lather <nikhillather02687@gmail.com>
2026-08-03 01:46:51 +0530

    updated

--- frontend/src/pages/shop/TrendingProducts.jsx
+++ frontend/src/pages/shop/TrendingProducts.jsx
@@ -1,4 +1,38 @@
+import { useState } from "react";
+import products from "../../data/product.json";
+import ProductCards from "./ProductCards";
+
 const TrendingProducts = () => {
-  return <div>TrendingProducts</div>;
+  const [visibleProducts, setVisibleProducts] = useState(8);
+
+  const loadMoreProducts = () => {
+    setVisibleProducts((prevCount) => prevCount + 4);
+  };
+
+  return (
+    <section className="section__container product__container">
+      <h2 className="section__header">Trending Products</h2>
+
+      <p className="section__subheader mb-12">
+        Discover the Hottest Picks! Elevate Your Style with Our Curated
+        Collection of Trending Women's Fashion Products.
+      </p>
+
+      {/* Products Card */}
+      <div className="mt-12">
+        <ProductCards products={products.slice(0, visibleProducts)} />
+      </div>
+
+      {/* Load More Products Button */}
+      <div className="product__btn">
+        {visibleProducts < products.length && (
+          <button onClick={loadMoreProducts} className="btn">
+            Load More
+          </button>
+        )}
+      </div>
+    </section>
+  );
 };
+
 export default TrendingProducts;
